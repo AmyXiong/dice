f5ed927d75c8754045949bd10127cee05f7a1dab
Amy Xiong <hafaszxdj98@gmail.com>
2014-04-03 19:43:23 -0400

    Added a feature that asks the user how many dice the user wants to roll.

--- dice.py
+++ dice.py
@@ -3,5 +3,6 @@ import inputLibrary
 
 dice = rand.random(1,7)
 side = inputLibrary.int_input("How many sides the die should have?")
+diceNumber = inputLibrary.int_input("How many dice do you want to roll?")
 
 print "The result of the dice is", dice
