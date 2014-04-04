36340d6cef7677e60d63339fb14aa3176e58bce3
Amy Xiong <hafaszxdj98@gmail.com>
2014-04-03 20:10:11 -0400

    Added a feature that asks that how many sides each die has and rolls them.

--- dice.py
+++ dice.py
@@ -1,9 +1,9 @@
 import random
 import inputLibrary
 
-side = inputLibrary.int_input("How many sides the die should have?")
 diceNumber = inputLibrary.int_input("How many dice do you want to roll?")
 
 for times in range(1,diceNumber+1):
+	side = inputLibrary.int_input("How many sides the die should have?")
 	dice = rand.random(1,side+1)
 	print "The result of the dice is", dice
