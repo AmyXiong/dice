672ccc46e5a250059664f68f203b50405485e9f5
Amy Xiong <hafaszxdj98@gmail.com>
2014-04-03 20:03:29 -0400

    Fixed the bug for the previous program.

--- dice.py
+++ dice.py
@@ -1,8 +1,9 @@
 import random
 import inputLibrary
 
-dice = rand.random(1,7)
 side = inputLibrary.int_input("How many sides the die should have?")
 diceNumber = inputLibrary.int_input("How many dice do you want to roll?")
 
-print "The result of the dice is", dice
+for times in range(1,diceNumber+1):
+	dice = rand.random(1,side+1)
+	print "The result of the dice is", dice
