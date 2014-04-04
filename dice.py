787ef005d568899d53a102068d0541a70638b4b4
Amy Xiong <hafaszxdj98@gmail.com>
2014-04-03 20:18:25 -0400

    Modified limits to side and number of dice and added shebang.

--- dice.py
+++ dice.py
@@ -1,8 +1,17 @@
+#! /usr/bin/env python
+
 import random
 import inputLibrary
 
 side = inputLibrary.int_input("How many sides the die should have?")
+while side < 1:
+	print "No one-sided dice are allowed.Try again"
+	side = inputLibrary.int_input("How many sides the die should have?")
+	
 diceNumber = inputLibrary.int_input("How many dice do you want to roll?")
+while diceNumber <= 0:
+	print "No negative number dice are allowed. Try again."
+	diceNumber = inputLibrary.int_input("How many dice do you want to roll?")
 
 for times in range(1,diceNumber+1):
 	dice = rand.random(1,side+1)
